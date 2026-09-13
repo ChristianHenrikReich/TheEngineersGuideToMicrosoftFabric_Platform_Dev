97e9f3486461aad2253e23dc85090f6eca0c89c7
ChristianHenrikReich <havehave@gmail.com>
2026-09-13 20:45:29 +0200

    Committing 1 item from workspace af4f17e2-63d4-409c-ac5e-d1e7381de0f2

--- solution/ingestion/customers_sql_databases/ingest_from_customers_databases.Notebook/notebook-content.py
+++ solution/ingestion/customers_sql_databases/ingest_from_customers_databases.Notebook/notebook-content.py
@@ -25,7 +25,7 @@
 # CELL ********************
 
 key_vault_name = "https://mugatokvdev2xdvgsdb4sxne.vault.azure.net/"
-connection_secret_name = "FabricReader-Dev-Dev"
+connection_secret_name = "FabricReader-Dev-Dev-Dev"
 
 
 # METADATA ********************
